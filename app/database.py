6fbfa07fc638fd1347df2ae9bb7cb032cfa8dc8e
Abhimanyu R B <abhimanyurbsa@gmail.com>
2025-03-07 13:10:34 +0530

    Made cheriya change to database.py - sookshicho

--- app/database.py
+++ app/database.py
@@ -1,14 +1,13 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from .config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-
 def get_db():
+    """Dependency to get the database session."""
     db = SessionLocal()
     try:
         yield db
